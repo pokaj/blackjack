eaf27fa97c25095e952b54393554a0cd3748b66a
EmmanuelKpeglo <emmanuel.kpeglo@turntabl.io>
2021-11-18 15:05:16 +0000

    added a deck class'

---
+++ src/src/Deck.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Deck {
+    private List<Card> cards = new ArrayList<>(52);
+
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card draw () {
+        Card card = cards.get(cards.size() - 1);
+        cards.remove(cards.size() - 1);
+        return card;
+    }
+
+}
